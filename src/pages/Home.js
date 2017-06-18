a832a88e5359115225c580f0e9cf5d98819e1597
Alan Tai <ayltai@gmail.com>
2017-06-18 21:03:19 +0800

    Implement Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@
 
 import React from 'react';
 import muiThemeable from 'material-ui/styles/muiThemeable';
+import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import LogoImage from '../images/logo.jpg';
 
 class Home extends React.Component {
     componentDidMount() : void {
@@ -11,7 +13,23 @@ class Home extends React.Component {
 
     render() : any {
         return (
-            <div>Home</div>
+            <div>
+                <Card className="homeItem">
+                    <CardMedia>
+                        <img
+                            src={LogoImage}
+                            style={{
+                                maxWidth : '100%'
+                            }} />
+                    </CardMedia>
+                    <CardTitle title="Fishing Club" />
+                    <CardText>
+                        <p>Welcome to our fishing club! We are going to hold our first ever fishing activity. Here you will find useful information in a nicely organized way.</p>
+                        <p>We try to include as much detailed information as we can. Do let us know if we miss any information that you need. If you are in a hurry, please check <a href="/others/preparations" title="Preparations">Preparations</a> page where you will learn how to better prepare yourself for the activity.</p>
+                        <p>Hope you will enjoy the activity.</p>
+                    </CardText>
+                </Card>
+            </div>
         );
     }
 }
